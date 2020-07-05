d30a9c285eab235b78e3fe685cc5154de51ec85d
Miguel <miguel.rafael@achievewithoutborders.com>
2020-07-05 15:46:57 +0800

    added coding style guide reference

---
+++ style_guide.js
@@ -0,0 +1,19 @@
+// camelCase
+let anotherVariable = 1
+
+// PascalCase
+let AnotherVariable = 1
+
+// Uppercase
+let Anothervariable = 1
+
+// snake_case
+let another_variable = 1;
+
+// kebab-case
+let another-variable = 1
+
+// MACRO_CARE
+let ANOTHER_VARIABLE = 1
+
+
